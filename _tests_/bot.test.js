eee9cdbc00fdf0bb6cfd2171df13bc8f3faaf847
MaxChekh <max.chekh1313@gmail.com>
2020-10-22 23:37:35 +0300

    init tests

---
+++ _tests_/bot.test.js
@@ -0,0 +1,16 @@
+//TODO tests;)
+/*
+const config = require('../config');
+const TelegrafTest = require('telegraf-test');
+const user = new TelegrafTest({
+    url: `${config.bot.webHook}`
+});
+
+user.setUser({
+    id: 522658339,
+    username: '@test'
+});
+test('Bot',async()=>{
+    const res = await user.sendMessageWithText('123');
+    expect(res.data.text).toEqual('<b>Меню</b>');
+});*/
